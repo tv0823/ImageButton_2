397dcf50cc1d48e19c144133b73b9fd523d10187
tv0823 <tv0823@bs.amalnet.k12.il>
2024-01-09 09:33:11 +0200

    finish

--- app/src/main/java/com/example/imagebutton_2/MainActivity.java
+++ app/src/main/java/com/example/imagebutton_2/MainActivity.java
@@ -3,12 +3,40 @@ package com.example.imagebutton_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import  java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView iV;
+    ImageButton clickimg;
+    Random rd = new Random();
+    int num;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        iV = findViewById(R.id.iV);
+        clickimg = findViewById(R.id.clickimg);
+    }
+
+    public void clicked(View view) {
+        num = rd.nextInt(3)+1;
+        if (num == 1)
+        {
+            iV.setImageResource(R.drawable.cat);
+            clickimg.setImageResource(R.drawable.one);
+        }
+        if (num == 2)
+        {
+            iV.setImageResource(R.drawable.monkey);
+            clickimg.setImageResource(R.drawable.two);
+        }
+        if (num == 3)
+        {
+            iV.setImageResource(R.drawable.dog);
+            clickimg.setImageResource(R.drawable.three);
+        }
     }
 }
